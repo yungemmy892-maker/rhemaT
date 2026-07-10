fc3970c5837b66d155af4dc2d6d9e103f951eaee
yungemmy892-maker <yungemmy892@gmail.com>
2026-07-10 21:16:23 +0100

    fix:Added static files and added whitenoise to requirement.txt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,7 +72,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # User-uploaded files (currently just profile avatars). Served by Django
